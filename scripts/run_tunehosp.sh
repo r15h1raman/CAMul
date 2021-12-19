c58d5f82ca748a23e2dfba0a1b5447efb1c4eec5
Gautham Gururajan <47530745+Gautham-G@users.noreply.github.com>
2021-12-19 15:49:14 -0500

    Update run_tunehosp.sh

--- scripts/run_tunehosp.sh
+++ scripts/run_tunehosp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
     
 i=1
-while [ $i -le 7 ]
-    echo Day $i
+while [ $i -le 30 ]
     do
-    python ./train_hosp2_tune.py -e 202149 -m deploy_week_49_tune -c True -d $i 
-    ((i++))
+    echo Day $i
+#     python ./train_hosp2_tune.py -e 202149 -m deploy_week_49_tune -c True -d $i 
+#     ((i++))
     done
 echo All done
