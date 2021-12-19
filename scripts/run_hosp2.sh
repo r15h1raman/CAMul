a2f315dc1af4037890abaa72a34f959cc3c6845b
Gautham Gururajan <47530745+Gautham-G@users.noreply.github.com>
2021-12-19 15:55:13 -0500

    Update run_hosp2.sh

--- scripts/run_hosp2.sh
+++ scripts/run_hosp2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
     
 i=1
-while [ $i -le 7 ]
+while [ $i -le 30 ]
     echo Day $i
     do
-    python ./train_hosp3.py -e 202149 -m deploy_week_49 -c True -d $i 
+    python ./train_hosp3.py -e 202150 -m deploy_week_50 -c True -d $i 
     ((i++))
     done
 echo All done
