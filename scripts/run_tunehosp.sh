10505922982094544816c6bf98a4635b506bbfbb
Gautham Gururajan <47530745+Gautham-G@users.noreply.github.com>
2021-12-22 00:59:58 -0500

    Update run_tunehosp.sh

--- scripts/run_tunehosp.sh
+++ scripts/run_tunehosp.sh
@@ -3,21 +3,21 @@
     for i in 1 2 3 4 5 6 7 8 9 10 
     do
     echo Day $i
-    python ./train_hosp3.py -e 202150 -m deploy_week_50 -c True -d $i --samples 1
+    python ./train_hosp3.py -e 202150 -m deploy_week_50_st -c True -d $i --samples 1
     ((i++))
     done
     
     for i in 11 12 13 14 15 16 17 18 19 20 
     do
     echo Day $i
-    python ./train_hosp3.py -e 202150 -m deploy_week_50 -c True -d $i --samples 3
+    python ./train_hosp3.py -e 202150 -m deploy_week_50_st -c True -d $i --samples 3
     ((i++))
     done
     
     for i in 21 22 23 24 25 26 27 28 29 30 
     do
     echo Day $i
-    python ./train_hosp3.py -e 202150 -m deploy_week_50 -c True -d $i --samples 5
+    python ./train_hosp3.py -e 202150 -m deploy_week_50_st -c True -d $i --samples 5
     ((i++))
     done
     
